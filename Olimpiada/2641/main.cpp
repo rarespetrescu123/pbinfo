33ee2e3dcc20c98664a69515930a5da7eba35a20
rarespetrescu123 <aprilie2009rares@gmail.com>
2023-08-05 16:16:52 +0300

    rezolvare problema 2641

--- Olimpiada/2641/main.cpp
+++ Olimpiada/2641/main.cpp
@@ -1,11 +1,49 @@
 #include <iostream>
+#include <fstream>
 
 using namespace std;
+ifstream fin("af.in");
+ofstream fout("af.out");
 
 int main()
 {
-    int n;
-    cin>>n;
-    cout<<(n-1)*n/2;
+    char semn,egal;
+    long long int n,nr1,nr2,nr3;
+    fin>>n;
+    for(int i=0;i<n;i++)
+    {
+        fin>>nr1>>semn>>nr2>>egal>>nr3;
+        if(semn=='+')
+        {
+            if(nr1+nr2==nr3)
+                fout<<"Adevarat";
+            else
+                fout<<"Fals";
+        }
+        if(semn=='-')
+        {
+            if(nr1-nr2==nr3)
+                fout<<"Adevarat";
+            else
+                fout<<"Fals";
+        }
+        if(semn=='x')
+        {
+            if(nr1*nr2==nr3)
+                fout<<"Adevarat";
+            else
+                fout<<"Fals";
+        }
+        if(semn==':')
+        {
+
+            if(nr2!=0 && nr1/nr2==nr3)
+                fout<<"Adevarat";
+            else
+                fout<<"Fals";
+        }
+        fout<<'\n';
+    }
+
     return 0;
 }
