84097fe971aeabcec1894fd19b628b63ca3fa24a
rarespetrescu123 <aprilie2009rares@gmail.com>
2023-08-19 17:17:10 +0300

    rezolvare 1875 pbinfo

--- Olimpiada/1875/main.cpp
+++ Olimpiada/1875/main.cpp
@@ -7,6 +7,27 @@ ofstream fout("platou1.out");
 
 int main()
 {
+    int n,lgsecv=0,lgsecvmax=0,elsecv=0,prec=-1;
+    while(fin>>n)
+    {
+        if(n==prec)
+            lgsecv++;
+        else
+        {
+            if(lgsecv>lgsecvmax)
+            {
+                lgsecvmax=lgsecv;
+                elsecv=prec;
+            }
+            else if(lgsecv==lgsecvmax && prec<elsecv)
+            {
+                elsecv=prec;
+            }
+            lgsecv=1;
+            prec=n;
 
+        }
+    }
+    fout<<lgsecvmax<<" "<<elsecv;
     return 0;
 }
