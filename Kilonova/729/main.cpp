d2a742b7bc95e43210a1b63f204bce0a6c2b3aa0
rarespetrescu123 <aprilie2009rares@gmail.com>
2023-09-11 18:42:22 +0300

    rezolvare 729 kilonova

--- Kilonova/729/main.cpp
+++ Kilonova/729/main.cpp
@@ -1,12 +1,32 @@
 #include <iostream>
 #include <fstream>
+#include <algorithm>
+
+#define MAXN 100
 
 using namespace std;
 ifstream fin("siruri.in");
 ofstream fout("siruri.out");
-
+int v[MAXN],vsortat[MAXN];
 int main()
 {
+    int n;
+
+    fin>>n;
+    for(int i=0;i<n;i++)
+    {
+        fin>>v[i];
+        vsortat[i]=v[i];
+    }
+    sort(vsortat,vsortat+n);
+    for(int i=0;i<n;i++)
+        for(int j=0;j<n;j++)
+        {
+            if(v[i]==vsortat[j])
+                fout<<j+1<<" ";
+        }
+
+
 
     return 0;
 }
